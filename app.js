e8ecff9e707c5cb738c68df5e66aba659254dc0e
Christopher João da Cruz Ferreira <christopherjoao2007@gmail.com>
2025-07-04 14:02:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true}))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine',"ejs")
-app.use(express.static('public'))
+app.use(express.static("public"))
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts)
 app.set('layout', 'base')
